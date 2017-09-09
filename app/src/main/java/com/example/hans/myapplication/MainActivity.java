3d3494acb3c66c42ce393250c2ccf6cbfc9a9e45
Johannes Herr <johannes.herr@gmail.com>
2017-09-09 16:54:30 +0200

    play pause position

--- app/src/main/java/com/example/hans/myapplication/MainActivity.java
+++ app/src/main/java/com/example/hans/myapplication/MainActivity.java
@@ -1,35 +1,35 @@
 package com.example.hans.myapplication;
 
+import android.annotation.SuppressLint;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.*;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+@SuppressLint("SetTextI18n")
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+        final SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+        final int max = 100_000;
+        seekBar.setMax(max);
 
 
         final List<File> podcasts = new LinkedList<>();
@@ -48,36 +48,55 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        Collections.sort(podcasts);
+        Collections.sort(podcasts, new Comparator<File>() {
+            @Override
+            public int compare(File lhs, File rhs) {
+                return num(rhs) - num(lhs);
+            }
+
+            private int num(File lhs) {
+                Pattern nbr = Pattern.compile("\\d+");
+                Matcher matcher = nbr.matcher(lhs.getName());
+                if (matcher.find()) {
+                    return Integer.parseInt(matcher.group(0));
+                } else {
+                    return 0;
+                }
+            }
+        });
 
         List<String> names = new LinkedList<>();
         for (File podcast : podcasts) names.add(podcast.getName());
 
         final MediaPlayer mediaPlayer = MediaPlayer.create(this, Uri.fromFile(podcasts.get(0)));
 
+        final File[] playing = {null};
         ListView listView = (ListView) findViewById(R.id.pods);
         listView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, names));
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                System.out.println("id = " + id);
                 File podcast = podcasts.get((int) id);
-                System.out.println("podcasts = " + podcast.getName());
 
                 TextView text = (TextView) findViewById(R.id.thetext);
 
                 try {
-                    if (mediaPlayer.isPlaying()) {
-                        text.setText("pause");
+                    boolean changeSong = podcast != playing[0];
+                    playing[0] = podcast;
+
+                    if (changeSong) {
+                        playNew(podcast, text, mediaPlayer, seekBar);
+
+                    } else if (mediaPlayer.isPlaying()) {
+                        text.setText("pause: " + podcast.getName());
                         mediaPlayer.pause();
 
-                    } else {
-                        mediaPlayer.stop();
-                        mediaPlayer.reset();
-                        mediaPlayer.setDataSource(MainActivity.this, Uri.fromFile(podcast));
-                        mediaPlayer.prepare();
-                        text.setText("playing");
+                    } else if (!mediaPlayer.isPlaying()) {
+                        text.setText("play: " + podcast.getName());
                         mediaPlayer.start();
+
+                    } else {
+                        throw new AssertionError();
                     }
 
                 } catch (IOException e) {
@@ -86,14 +105,34 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    int duration = mediaPlayer.getDuration();
+                    double relPos = progress / ((double) max);
+                    mediaPlayer.seekTo((int) (duration * relPos));
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
             }
         });
+    }
+
+    private void playNew(File podcast, TextView text, MediaPlayer mediaPlayer, SeekBar seekBar) throws IOException {
+        mediaPlayer.stop();
+        mediaPlayer.reset();
+        mediaPlayer.setDataSource(MainActivity.this, Uri.fromFile(podcast));
+        mediaPlayer.prepare();
+        text.setText("playing: " + podcast.getName());
+        mediaPlayer.start();
+
+        seekBar.setProgress(0);
     }
 
     @Override
